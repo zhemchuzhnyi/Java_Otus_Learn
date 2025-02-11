7ef333bd91f2caf22607aeac780d7c90947d4da9
Otus_PC <a_reshkin@inbox.ru>
2025-02-11 21:32:35 +0300

    Lesson_2

---
+++ src/main/java/Lesson2.java
@@ -0,0 +1,40 @@
+import static java.lang.Integer.sum;
+import static java.lang.Math.max;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+
+        int i = 130; //byte - от минус 128 до 127 это 1 байт
+        char o = 'h';
+        boolean y = true;
+        System.out.println(y);
+
+
+        Cat cat = new Cat();
+        Cat ttt = new Cat();
+        Dog r = new Dog();
+
+        Integer x = 120;
+        Integer xx = max(11,120);
+
+        Integer iy = 123;
+        Integer yu = 125;
+
+        // Integer oo = max(iy,yu);
+        Integer oo2 = sum(iy,yu);
+
+        byte m = 6;
+        int m2 = m; // лежит 6 // 2 байта
+
+        byte m3 = (byte) m2;
+
+
+
+
+
+
+
+
+
+    }
+}
