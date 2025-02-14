b649c9495e335a7bbba4e8a8759cceb6a15de343
Otus_PC <a_reshkin@inbox.ru>
2025-02-14 16:50:45 +0300

    Lesson_3_1 Рекурсия

---
+++ src/main/java/Lesson3_1.java
@@ -0,0 +1,14 @@
+public class Lesson3_1 {
+    public static void countDown(int n) {
+        if (n == 0) { // Базовый случай: если n равно 0
+            System.out.println("Готово!");
+        } else {
+            System.out.println(n);
+            countDown(n - 1); // Рекурсивный вызов с n - 1
+        }
+    }
+
+    public static void main(String[] args) {
+        countDown(5); // Запуск с числа 5
+    }
+}
