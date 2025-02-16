4ef88d144edaa5295e784a1a5e5bb9b7e475dd46
zhemchuzhnyi <a_reshkin@inbox.ru>
2025-02-16 22:56:23 +0300

    Lesson_3 Update equals

---
+++ src/main/java/Lesson3_2.java
@@ -0,0 +1,9 @@
+public class Lesson3_2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        if (a == b) {
+            System.out.println("a и b равны");
+        }
+    }
+}
